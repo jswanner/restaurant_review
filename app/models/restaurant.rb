57f8cf210bb6a71a10912d9abddcf850200bec15
Jacob Swanner <swanner@gmail.com>
2009-09-02 00:54:12 -0400

    moving the call to Yelp out of find_restaurants in an attempt to make it more readable

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,24 +6,37 @@ class Restaurant
   attr_reader :address, :name, :rating, :reviews
 
   def self.find_restaurants
-    client = Yelp::Client.new
-    req = Yelp::Review::Request::Location.new(
-      :zipcode => "32839",
-      :term => 'restaurants',
-      :yws_id => ENV['YWSID'])
-    resp = client.search(req)
+    resp = call_yelp
     restaurants = resp["businesses"].map do |bus|
-      addr = build_address(bus)
-      rating = (bus["reviews"].inject(0.0){ |s, r| s += r["rating"] } / bus["reviews"].length)
-      reviews = bus["reviews"].inject([]){ |a, r| a << r["text_excerpt"] }
-      Restaurant.new(addr, bus["name"], rating, reviews)
+      Restaurant.new(
+        build_address(bus),
+        bus["name"],
+        build_rating(bus["reviews"]),
+        build_reviews(bus["reviews"]))
     end
   end
 
   private
 
+  def self.call_yelp
+    client = Yelp::Client.new
+    req = Yelp::Review::Request::Location.new(
+      :zipcode => "32839",
+      :term => 'restaurants',
+      :yws_id => ENV['YWSID'])
+    client.search(req)
+  end
+
   def self.build_address(bus)
-    "#{bus["address1"]} #{bus["city"]}, #{bus["state"]} #{bus["zip"]}"
+    "#{bus["address1"]}, #{bus["city"]}, #{bus["state"]} #{bus["zip"]}"
+  end
+  
+  def self.build_rating(reviews)
+    (reviews.inject(0.0){ |s, r| s += r["rating"] } / reviews.length)
+  end
+
+  def self.build_reviews(reviews)
+    reviews.inject([]){ |a, r| a << r["text_excerpt"] }
   end
     
 end
