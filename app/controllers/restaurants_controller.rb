b7d43b91d8b93227d59fce2630ea129b50ade9bc
Jacob Swanner <swanner@gmail.com>
2009-09-09 23:35:40 -0400

    fixing error in restaurants controller related to renaming of find_restaurants method

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,7 @@ class RestaurantsController < ApplicationController
   respond_to :json, :xml, :amf
 
   def index
-    @restaurants = Restaurant.find_restaurants
+    @restaurants = Restaurant.find_all
     respond_with(@restaurants) do |format|
       format.json { render :json => @restaurants }
       format.amf { render :amf => @restaurants }
